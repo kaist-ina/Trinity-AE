f0631b184efc0bc35c3ff25a50e6a4589654c238
jhpark676 <woguddlrj676@kaist.ac.kr>
2026-04-24 03:57:53 +0000

    increase max benchmarks

--- trinity/config.py
+++ trinity/config.py
@@ -33,7 +33,7 @@ class OptimizeConfig:
     optimizer_timeout_s: int = 3600
     backend_timeout_s: Optional[int] = None
     backend_optimized_benchmark: bool = True
-    backend_max_benchmarks: int = 64
+    backend_max_benchmarks: int = 512
     backend_seed_samples: int = 16
     backend_local_refine_budget: int = 16
     backend_local_refine_top_k: int = 1
